42444e0ec817b61299f11cbf31c53880c3cceeeb
francesco <francesco.sangiovanni.7@gmail.com>
2022-12-19 13:19:21 +0100

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRoute from "./routes/userRoute";
 import dotenv from "dotenv/config";
 
 const PORT = process.env.PORT;
@@ -11,6 +12,11 @@ mongoose.connect("mongodb://localhost:27017/user_database")
 .catch((err) => console.log("Couldn't connect to the database"));
 
 app.use(express.json());
+app.use("/user", userRoute);
+
+app.get("*", (req, res) => {
+    res.send("Resource not available")
+});
 
 app.listen(() => console.log("Listening on port "+PORT));
 
