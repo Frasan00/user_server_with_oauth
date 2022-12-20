73bd5fbc5b3c0fe73ed3a125013b32ed32a27627
Frasan00 <francesco.sangiovanni.7@gmail.com>
2022-12-20 14:01:11 +0100

    first commit

--- src/utilities/checkIfExists.ts
+++ src/utilities/checkIfExists.ts
@@ -1,13 +1,13 @@
 import Session from "../database/Session";
 import User from "../database/User";
 
-async function checkSession (userName: string){
-    const check = await Session.findOne({userName: userName});
+function checkSession (userName: string){
+    const check = Session.findOne({userName: userName});
     return check;
 };
 
-async function checkUser (userName: string){
-    const check = await User.findOne({userName: userName});
+function checkUser (userName: string){
+    const check = User.findOne({userName: userName});
     return check;
 };
 
